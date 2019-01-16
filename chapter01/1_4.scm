916f38d3c3b5965359e85be99c17299f6c473c06
Michael Liu <thinker1990@users.noreply.github.com>
2019-01-16 10:59:29 +0800

    add exercise 1.5

--- chapter01/1_4.scm
+++ chapter01/1_4.scm
@@ -1,3 +1,10 @@
+#lang racket
+
 ;; if b is positive, then a + b, otherwise a - b
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
+
+
+;; test cases
+(a-plus-abs-b 1 2)   ;; 3
+(a-plus-abs-b 1 -2)  ;; 3
---
+++ chapter01/1_5.scm
@@ -0,0 +1,23 @@
+#lang racket
+
+(define (p) (p))
+
+(define (test x y)
+  (if (= x 0)
+      0
+      y))
+
+
+;; test cases
+(test 0 (p))
+;; applicative-order: the interpreter will enter infinite loop.
+;;     Because applicative-order evaluation will evaluate the
+;;     arguments first. When encounter the second argument (p),
+;;     infinite recursive happens, the interpreter will freeze.
+;;
+;; normal-order: the interpreter will print result 0 immediately.
+;;     Because normal-order evaluation would not evaluate the operands
+;;     until their values were needed. When evaluate the if expression,
+;;     the predicate part, (= x 0) evaluated to true. the interpreter 
+;;     then evaluates the consequent part and returns its value, 0.
+;;     The alternative part, (p) is not evaluted at all.
