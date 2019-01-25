ae84f800fbdb7f1016af5b78ec95b5ec6956c530
Michael Liu <thinker1990@users.noreply.github.com>
2019-01-25 15:54:32 +0800

    add exercise 1.20

---
+++ chapter01/1_20.scm
@@ -0,0 +1,21 @@
+#lang racket
+
+(define (gcd a b)
+    (if (= b 0)
+        a
+        (gcd b (remainder a b))))
+
+
+
+;;  (gcd 206 40) = (gcd 40 (remainder 206 40)) = (gcd 40 6)
+;;               = (gcd 6 (remainder 40 6)) = (gcd 6 4)
+;;               = (gcd 4 (remainder 6 4)) = (gcd 4 2) 
+;;               = (gcd 2 (remainder 4 2)) = (gcd 2 0)
+;;               = 2
+;;
+;;  The remainder operations are performed 4 times in both
+;;  normal-order evaluation and applicative-order evaluation.
+;;  Because predict clause of if should be evaluated immediately.
+;;  
+;;  Other solution: https://github.com/qiao/sicp-solutions/blob/master/chapter1/1.20.scm
+;;  Which I think is incorrect.
