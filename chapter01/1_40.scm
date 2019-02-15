f017963ef4f6ed827c02607444d43894f5b4e9f3
Michael Liu <thinker1990@users.noreply.github.com>
2019-02-15 14:33:54 +0800

    add exercise 1.40
    fix bug of exercise 1.39

---
+++ chapter01/1_40.scm
@@ -0,0 +1,32 @@
+#lang sicp
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+    (define (close-enough? v1 v2)
+        (< (abs (- v1 v2)) tolerance))
+    (define (try guess)
+        (let ((next (f guess))) 
+             (if (close-enough? guess next) 
+                 next 
+                 (try next))))
+    (try first-guess))
+
+(define dx 0.00001)
+(define (deriv g)
+    (lambda (x) (/ (- (g (+ x dx)) (g x)) dx)))
+
+(define (newton-transform g) 
+    (lambda (x) (- x (/ (g x) ((deriv g) x)))))
+
+(define (newtons-method g guess) 
+    (fixed-point (newton-transform g) guess))
+
+(define (cubic a b c)
+    (lambda (x) (+ (* x x x) (* a (* x x)) (* b x) c)))
+
+
+;; test cases
+(newtons-method (cubic 0 0 -8) 1)
+(newtons-method (cubic 0 0 -27) 1)
+(newtons-method (cubic 0 0 -64) 1)
+(newtons-method (cubic 0 0 -125) 1)
