1cf15d8c630f52382e0112f5e8ef178d1f78e507
thinker1990 <liumkscut@gmail.com>
2019-02-27 19:53:16 +0800

    add exercise 2.15

---
+++ chapter02/2_15.scm
@@ -0,0 +1,4 @@
+#lang sicp
+
+;; Eva is right, since the error isn't reintroduced into the result in par2 as it is in par1.
+;; Solution from: http://community.schemewiki.org/?sicp-ex-2.14-2.15-2.16
