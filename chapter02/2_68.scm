90710e9382630b33fc0f11473ce5e913cb186174
Michael Liu <thinker1990@users.noreply.github.com>
2019-03-08 11:53:03 +0800

    update exercise 2.67 ~ 2.69 to use racket's modular functionality

--- chapter02/2_68.scm
+++ chapter02/2_68.scm
@@ -1,39 +1,7 @@
 #lang racket
+(provide (all-defined-out))
 
-(define (make-leaf symbol weight)
-  (list 'leaf symbol weight))
-
-(define (leaf? object)
-  (eq? (car object) 'leaf))
-
-(define (symbol-leaf x)
-  (cadr x))
-
-(define (weight-leaf x)
-  (caddr x))
-
-(define (make-code-tree left right)
-  (list left
-        right
-        (append (symbols left)
-                (symbols right))
-        (+ (weight left) (weight right))))
-
-(define (left-branch tree)
-  (car tree))
-
-(define (right-branch tree)
-  (cadr tree))
-
-(define (symbols tree)
-  (if (leaf? tree)
-      (list (symbol-leaf tree))
-      (caddr tree)))
-
-(define (weight tree)
-  (if (leaf? tree)
-      (weight-leaf tree)
-      (cadddr tree)))
+(require "2_67.scm")
 
 (define (encode message tree)
   (if (null? message)
@@ -55,16 +23,6 @@
 
 
 ;; test cases
-(define sample-tree
-  (make-code-tree 
-   (make-leaf 'A 4)
-   (make-code-tree
-    (make-leaf 'B 2)
-    (make-code-tree 
-     (make-leaf 'D 1)
-     (make-leaf 'C 1)))))
+(define message '(A D A B B C A))
 
-(define sample-message 
-  '(A D A B B C A))
-
-(encode sample-message sample-tree)  ;; '(0 1 1 0 0 1 0 1 0 1 1 1 0)
+(encode message sample-tree)  ;; '(0 1 1 0 0 1 0 1 0 1 1 1 0)
