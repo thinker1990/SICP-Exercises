5c936b2bb9e014d40328e5b46d85dd8a10be0d64
Michael Liu <thinker1990@users.noreply.github.com>
2019-03-28 10:19:48 +0800

    add exercise 3.31

---
+++ chapter03/3_31.scm
@@ -0,0 +1,5 @@
+#lang sicp
+
+;; Answer from: https://wizardbook.wordpress.com/2010/12/17/exercise-3-31/
+;; Without executing the procedure immediately the system would never actually start - nothing
+;; would be added to the agenda, no actions would occur and no signals would propagate.
