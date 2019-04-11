bc423fec5d7d75610341d48c3074ae2a0c25b669
thinker1990 <liumkscut@gmail.com>
2019-04-11 20:18:40 +0800

    add exercise 3.67

---
+++ chapter03/3_67.scm
@@ -0,0 +1,12 @@
+#lang sicp
+
+(define (pairs s t)
+  (cons-stream 
+    (list (stream-car s) (stream-car t))
+    (interleave 
+      (interleave 
+        (stream-map (lambda (x) (list (stream-car s) x)) 
+                    (stream-cdr t)) 
+        (stream-map (lambda (x) (list (stream-car t) x)) 
+                    (stream-cdr s)))
+      (pairs (stream-cdr s) (stream-cdr t)))))
