ae3e75cf4da06ceb77c7d7bb1c521b10397c5fb4
thinker1990 <liumkscut@gmail.com>
2019-04-12 21:02:41 +0800

    add exercise 3.71

---
+++ chapter03/3_71.scm
@@ -0,0 +1,19 @@
+#lang sicp
+
+(define (cube-sum pair) 
+  (+ (cube (car pair))
+     (cube (cadr pair))))
+
+(define cube-sums 
+  (stream-map cube-sum 
+              (weighted-pairs integers integers cube-sum)))
+
+(define (consecutive-equal s) 
+  (let ((first (stream-car s))
+        (second (stream-car (stream-cdr s))))
+    (if (eq? first second)
+        (cons-stream second 
+                     (consecutive-equal (stream-cdr s))) 
+        (consecutive-equal (stream-cdr (stream-cdr s))))))
+
+(consecutive-equal cube-sums)
