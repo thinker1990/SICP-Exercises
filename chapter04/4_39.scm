42b63e7857d331aef9f90140efcf466f4d6179d8
Michael Liu <thinker1990@users.noreply.github.com>
2019-05-06 09:58:53 +0800

    add exercise 4.39

---
+++ chapter04/4_39.scm
@@ -0,0 +1,27 @@
+#lang racket
+
+;; The order of the restrictions does NOT affect the answer. 
+;; it DO affect the time to find an answer.
+
+(define (multiple-dwelling)
+  (let ((baker (amb 1 2 3 4 5))
+        (cooper (amb 1 2 3 4 5))
+        (fletcher (amb 1 2 3 4 5))
+        (miller (amb 1 2 3 4 5))
+        (smith (amb 1 2 3 4 5)))
+    (require (not (= fletcher 5)))
+    (require (not (= fletcher 1)))
+    (require (not (= cooper 1)))
+    (require (not (= baker 5)))
+    (require (> miller cooper))
+    (require 
+     (not (= (abs (- fletcher cooper)) 1)))
+    (require
+     (not (= (abs (- smith fletcher)) 1)))
+    (require 
+     (distinct? (list baker cooper fletcher miller smith)))
+    (list (list 'baker baker)
+          (list 'cooper cooper)
+          (list 'fletcher fletcher)
+          (list 'miller miller)
+          (list 'smith smith))))
