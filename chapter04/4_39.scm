bc5c5bcd1b11aa33bfe9462df65d013e341eeb91
Michael Liu <thinker1990@users.noreply.github.com>
2019-05-06 09:59:51 +0800

    update exercise 4.39

--- chapter04/4_39.scm
+++ chapter04/4_39.scm
@@ -2,7 +2,7 @@
 
 ;; The order of the restrictions does NOT affect the answer. 
 ;; it DO affect the time to find an answer.
-
+;; For more detailed answer, see: http://community.schemewiki.org/?sicp-ex-4.39
 (define (multiple-dwelling)
   (let ((baker (amb 1 2 3 4 5))
         (cooper (amb 1 2 3 4 5))
