fb71c0f4972cad653716000a546846e7ec5f37a8
Michael Liu <thinker1990@users.noreply.github.com>
2019-05-11 16:15:39 +0800

    add exercise 4.51

---
+++ chapter04/4_51.scm
@@ -0,0 +1,14 @@
+#lang racket
+
+(define (pernamenant-set? expr) 
+  (tagged-list? expr 'pernamenant-set!)) 
+
+(define (analyze-permanent-assignment exp)
+  (let ((var (assignment-variable exp))
+        (vproc (analyze (assignment-value exp))))
+    (lambda (env succeed fail)
+      (vproc env
+             (lambda (val fail2)
+               (set-variable-value! var val env)
+               (succeed 'ok fail2))
+             fail))))
