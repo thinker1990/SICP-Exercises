656c70be6a372bd07861959d2f4c49b95c744fba
Michael Liu <thinker1990@users.noreply.github.com>
2019-06-03 20:50:10 +0800

    add exercise 4.78

---
+++ chapter04/4_78.scm
@@ -0,0 +1,4 @@
+#lang sicp
+
+;; I can not work out the exercise.
+;; For more information, see: https://github.com/cxphoe/SICP-solutions/tree/master/Chapter%204-Metalinguistic%20Abstraction/4.Logical%20Programing/4.78
