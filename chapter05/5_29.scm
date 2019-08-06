4e59f0981e24fae819a21384ee2823723eba055c
Michael Liu <thinker1990@users.noreply.github.com>
2019-08-06 10:17:33 +0800

    add exercise 5.29

---
+++ chapter05/5_29.scm
@@ -0,0 +1,13 @@
+n   total-pushes    maximum-pushes 
+1         18                11 
+2         78                19 
+3        138                27 
+4        258                35 
+5        438                43 
+
+;; 1. 
+;; the maximu-pushes is: 8n + 3 
+
+;; 2. 
+;; S(n) = S(n-1) + S(n-2) + 42 
+;; S(n) = 60Fib(n+1) - 42 
